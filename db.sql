f45a4d0780c9e1de4c7e8ef8556f60578ebc2653
Douglas <douglascacc2@gmail.com>
2024-05-23 16:16:45 -0600

    add file for database

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE sysmedicDB
+
+USE sysmedicDB
+
+CREATE TABLE IF NOT EXISTS user (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    token VARCHAR(255) DEFAULT NULL,
+    is_confirmed TINYINT(1) DEFAULT 0
+) 
